20bf3bf5a0f86b04adca8410e9180b1523d1a95f
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-03 13:14:35 +0900

    [feat] 버튼 클릭 시 숫자 랜덤으로 보이기

--- app/src/main/java/com/example/prac_android/MainActivity.kt
+++ app/src/main/java/com/example/prac_android/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.prac_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
+        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        resultText.text = randomInt.toString()
     }
 }
