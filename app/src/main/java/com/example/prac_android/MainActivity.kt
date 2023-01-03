ab39b3fc23af4d037599890dedaab5fed7c82971
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-03 14:54:06 +0900

    [feat] dice imageview 추가

--- app/src/main/java/com/example/prac_android/MainActivity.kt
+++ app/src/main/java/com/example/prac_android/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.prac_android
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,21 +13,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.roll_button)
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        val resetButton: Button = findViewById(R.id.reset_button)
+        //val countUpButton: Button = findViewById(R.id.count_up_button)
+        //val resetButton: Button = findViewById(R.id.reset_button)
 
         rollButton.setOnClickListener { rollDice() }
-        countUpButton.setOnClickListener{ countUp() }
-        resetButton.setOnClickListener { reset() }
+        //countUpButton.setOnClickListener{ countUp() }
+        //resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
+        val diceImage: ImageView = findViewById(R.id.dice_img)
+        val drawableResource =when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
         Toast.makeText(this, "Roll Dice", Toast.LENGTH_SHORT).show()
-        resultText.text = randomInt.toString()
-    }
 
+    }
+/*
     private fun countUp() {
         val resultText: TextView = findViewById(R.id.result_text)
         Toast.makeText(this, "Count Up", Toast.LENGTH_SHORT).show()
@@ -47,4 +56,6 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show()
         resultText.text = "0"
     }
+
+ */
 }
