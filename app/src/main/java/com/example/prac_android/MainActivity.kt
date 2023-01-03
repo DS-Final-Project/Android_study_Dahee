f18a78456dde8f0607f1efaa63d77c598546135c
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-03 14:21:58 +0900

    [feat] 버튼 클릭 시 reset

--- app/src/main/java/com/example/prac_android/MainActivity.kt
+++ app/src/main/java/com/example/prac_android/MainActivity.kt
@@ -14,9 +14,11 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         val countUpButton: Button = findViewById(R.id.count_up_button)
+        val resetButton: Button = findViewById(R.id.reset_button)
 
         rollButton.setOnClickListener { rollDice() }
         countUpButton.setOnClickListener{ countUp() }
+        resetButton.setOnClickListener { reset() }
     }
 
     private fun rollDice() {
@@ -38,5 +40,11 @@ class MainActivity : AppCompatActivity() {
                 resultText.text = resultInt.toString()
             }
         }
+    }
+
+    private fun reset() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show()
+        resultText.text = "0"
     }
 }
