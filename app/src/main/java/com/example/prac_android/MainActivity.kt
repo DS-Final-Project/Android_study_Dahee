58d2f7b85648f73f35ed78551e62f9ab249c9731
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-09 15:12:54 +0900

    [feat] TextView에 click listener 추가

--- app/src/main/java/com/example/prac_android/MainActivity.kt
+++ app/src/main/java/com/example/prac_android/MainActivity.kt
@@ -18,6 +18,10 @@ class MainActivity : AppCompatActivity() {
             //it은 done 버튼을 의미
             addNickName(it)
         }
+        findViewById<TextView>(R.id.tv_nickname).setOnClickListener {
+            //it은 nicknameTextView를 의미
+            updateNickName(it)
+        }
     }
 
     private fun addNickName(view: View) {
@@ -34,5 +38,20 @@ class MainActivity : AppCompatActivity() {
         //done 버튼 클릭 시 키보드 숨기기
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+    }
+
+    private fun updateNickName(view: View) {
+        val editText = findViewById<EditText>(R.id.et_nickname)
+        val doneButton = findViewById<Button>(R.id.btn_done)
+
+        editText.visibility = View.VISIBLE
+        doneButton.visibility = View.VISIBLE
+        //view는 nicknameTextView를 의미
+        view.visibility = View.GONE
+        //editText에 포커스 맞추기
+        editText.requestFocus()
+        //키보드 보이게 하기
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(editText, 0)
     }
 }
