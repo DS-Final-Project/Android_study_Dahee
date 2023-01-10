6c4a7eeaaee7dba3bf3ee58148eb8d7506f76c18
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-10 14:11:55 +0900

    [feat] ColorMyViews UI

--- app/src/main/java/com/example/prac_android/MainActivity.kt
+++ app/src/main/java/com/example/prac_android/MainActivity.kt
@@ -13,45 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.btn_done).setOnClickListener {
-            //it은 done 버튼을 의미
-            addNickName(it)
-        }
-        findViewById<TextView>(R.id.tv_nickname).setOnClickListener {
-            //it은 nicknameTextView를 의미
-            updateNickName(it)
-        }
     }
 
-    private fun addNickName(view: View) {
-        //여기서 view는 done 버튼의 객체를 의미
-        val editText = findViewById<EditText>(R.id.et_nickname)
-        val nicknameTextView = findViewById<TextView>(R.id.tv_nickname)
-
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        //view는 done 버튼을 의미
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
-
-        //done 버튼 클릭 시 키보드 숨기기
-        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
-    }
-
-    private fun updateNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.et_nickname)
-        val doneButton = findViewById<Button>(R.id.btn_done)
-
-        editText.visibility = View.VISIBLE
-        doneButton.visibility = View.VISIBLE
-        //view는 nicknameTextView를 의미
-        view.visibility = View.GONE
-        //editText에 포커스 맞추기
-        editText.requestFocus()
-        //키보드 보이게 하기
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.showSoftInput(editText, 0)
-    }
 }
