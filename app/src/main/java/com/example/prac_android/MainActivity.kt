5bcdef5e06ce912b002eac2f71bfb0af3ff8b581
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-12 01:07:20 +0900

    [feat] AboutMe에 Data binding 적용

--- app/src/main/java/com/example/prac_android/MainActivity.kt
+++ app/src/main/java/com/example/prac_android/MainActivity.kt
@@ -1,53 +1,62 @@
 package com.example.prac_android
 
-import android.graphics.Color
+import android.content.Context
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.databinding.DataBindingUtil
+import com.example.prac_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Kim da hee")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myName = myName
 
-        setListeners()
-    }
-    private fun makeColored(view: View) {
-        when(view.id) {
-            //TextView 클릭 시
-            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
-            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
-            R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
-            R.id.box_four_text -> view.setBackgroundColor(Color.MAGENTA)
-            R.id.box_five_text -> view.setBackgroundColor(Color.BLUE)
-            //Button 클릭 시
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
-            else -> view.setBackgroundColor(Color.LTGRAY)
+        binding.btnDone.setOnClickListener {
+            //it은 done 버튼을 의미
+            addNickName(it)
+        }
+        binding.tvNickname.setOnClickListener {
+            //it은 nicknameTextView를 의미
+            updateNickName(it)
         }
     }
-    private fun setListeners() {
-        val boxOneText = findViewById<TextView>(R.id.box_one_text)
-        val boxTwoText = findViewById<TextView>(R.id.box_two_text)
-        val boxThreeText = findViewById<TextView>(R.id.box_three_text)
-        val boxFourText = findViewById<TextView>(R.id.box_four_text)
-        val boxFiveText = findViewById<TextView>(R.id.box_five_text)
 
-        val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
-
-        val redButton = findViewById<Button>(R.id.red_button)
-        val yellowButton = findViewById<Button>(R.id.yellow_button)
-        val greenButton = findViewById<Button>(R.id.green_button)
-
-        val clickableViews: List<View> = listOf(boxOneText,boxTwoText,boxThreeText
-            ,boxFourText,boxFiveText,rootConstraintLayout,redButton,yellowButton,greenButton)
-
-        for (item in clickableViews) {
-            item.setOnClickListener{makeColored(it)}
+    private fun addNickName(view: View) {
+        //여기서 view는 done 버튼의 객체를 의미
+        binding.apply {
+            myName?.nickname = etNickname.text.toString()
+            //바인딩 된 데이터가 변경 되었을 때 UI를 refresh 해준다.
+            invalidateAll()
+            etNickname.visibility = View.GONE
+            btnDone.visibility = View.GONE
+            tvNickname.visibility = View.VISIBLE
         }
+
+        //done 버튼 클릭 시 키보드 숨기기
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethodManager.hideSoftInputFromWindow(binding.btnDone.windowToken,0)
+    }
+
+    private fun updateNickName(view: View) {
+
+        binding.apply {
+            etNickname.visibility = View.VISIBLE
+            btnDone.visibility = View.VISIBLE
+            etNickname.visibility = View.GONE
+            etNickname.requestFocus()
+        }
+
+        //키보드 보이게 하기
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.showSoftInput(binding.etNickname, 0)
     }
 }
