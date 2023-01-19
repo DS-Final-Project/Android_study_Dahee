5a34d289cce0fb7d03c04a11b3f1c2c898a06663
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-19 15:30:31 +0900

    [feat] navigation popUpTo 적용

--- app/src/main/java/com/example/prac_android/GameOverFragment.kt
+++ app/src/main/java/com/example/prac_android/GameOverFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.prac_android.databinding.FragmentGameOverBinding
 
 class GameOverFragment : Fragment() {
@@ -14,6 +15,9 @@ class GameOverFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? { // Inflate the layout for this fragment
         binding = FragmentGameOverBinding.inflate(inflater,container,false)
+        binding.tryAgainButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(R.id.action_gameOverFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
--- app/src/main/java/com/example/prac_android/GameWonFragment.kt
+++ app/src/main/java/com/example/prac_android/GameWonFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.prac_android.databinding.FragmentGameWonBinding
 
 class GameWonFragment : Fragment() {
@@ -14,6 +15,9 @@ class GameWonFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? { // Inflate the layout for this fragment
         binding = FragmentGameWonBinding.inflate(inflater,container,false)
+        binding.nextMatchButton.setOnClickListener { view : View ->
+            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+        }
         return binding.root
     }
 }
