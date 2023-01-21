afd9d95fae34159ac353474d9bc5e05ab603977c
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-21 22:45:31 +0900

    [feat] AboutFragment 추가

--- app/src/main/java/com/example/prac_android/TitleFragment.kt
+++ app/src/main/java/com/example/prac_android/TitleFragment.kt
@@ -3,9 +3,13 @@ package com.example.prac_android
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.prac_android.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
@@ -19,6 +23,18 @@ class TitleFragment : Fragment() {
                 view : View ->
             view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 }
