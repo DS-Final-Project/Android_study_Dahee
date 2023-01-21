58bd3581d44c1614b9bea7698e6f2c7a40867516
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-22 00:53:24 +0900

    [feat] navigation drawer 추가

---
+++ app/src/main/java/com/example/prac_android/RulesFragment.kt
@@ -0,0 +1,20 @@
+package com.example.prac_android
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.prac_android.databinding.FragmentRulesBinding
+
+class RulesFragment : Fragment() {
+    private lateinit var binding: FragmentRulesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? { // Inflate the layout for this fragment
+        binding = FragmentRulesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
