c8596be285b247eb6dae7523665c8b9fd537cba2
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-01-31 23:25:05 +0900

    [feat] DessertTimer setting

---
+++ app/src/main/java/com/example/prac_android/DessertTimer.kt
@@ -0,0 +1,3 @@
+package com.example.prac_android
+
+class DessertTimer {}
--- app/src/main/java/com/example/prac_android/MainActivity.kt
+++ app/src/main/java/com/example/prac_android/MainActivity.kt
@@ -3,30 +3,14 @@ package com.example.prac_android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.navigation.findNavController
-import androidx.navigation.ui.NavigationUI
 import com.example.prac_android.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-
-        drawerLayout = binding.drawerLayout
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        NavigationUI.setupWithNavController(binding.navView, navController)
-        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
-        setContentView(binding.root)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
     }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.myNavHostFragment)
-        return NavigationUI.navigateUp(navController,drawerLayout)
-    }
-
 }
