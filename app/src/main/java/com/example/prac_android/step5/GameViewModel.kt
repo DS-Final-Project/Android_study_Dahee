f7d5e2c08212f876024fcd3a8cb8170e74906dfa
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-02-08 23:44:30 +0900

    [feat] Backing property 적용

--- app/src/main/java/com/example/prac_android/step5/GameViewModel.kt
+++ app/src/main/java/com/example/prac_android/step5/GameViewModel.kt
@@ -1,15 +1,20 @@
 package com.example.prac_android.step5
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import timber.log.Timber
 
 class GameViewModel : ViewModel() {
     // The current word
-    var word = MutableLiveData<String>()
+    private var _word = MutableLiveData<String>()
+    val word: LiveData<String>
+    get() = _word
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private var _score = MutableLiveData<Int>()
+    val score: LiveData<Int>
+    get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -49,8 +54,8 @@ class GameViewModel : ViewModel() {
         nextWord()
         Timber.tag("GameViewModel").i("GameViewModel created!")
 
-        word.value = ""
-        score.value = 0
+        _word.value = ""
+        _score.value = 0
     }
 
     /**
@@ -59,20 +64,20 @@ class GameViewModel : ViewModel() {
     private fun nextWord() {
         if (wordList.isNotEmpty()) {
             //Select and remove a word from the list
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
     /** Methods for buttons presses **/
 
     //데이터 처리를 위한 코드 포함
     fun onSkip() {
-        score.value = (score.value)?.minus(1)
+        _score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     //데이터 처리를 위한 코드 포함
     fun onCorrect() {
-        score.value = (score.value)?.plus(1)
+        _score.value = (score.value)?.plus(1)
         nextWord()
     }
 
