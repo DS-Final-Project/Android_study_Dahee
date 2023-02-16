6c41b1c9f9ff54e3ccb173a1da8684499ef87cd5
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-02-16 15:54:08 +0900

    [feat] Entity 생성

--- app/src/main/java/com/example/prac_android/step6/database/SleepNight.kt
+++ app/src/main/java/com/example/prac_android/step6/database/SleepNight.kt
@@ -1,3 +1,21 @@
 package com.example.prac_android.step6.database
 
-class SleepNight {}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "daily_sleep_quality_table")
+data class SleepNight (
+    //nightId를 기본 키로 식별하려면 @PrimarKey를 사용
+    //room이 각 entity에 대한 ID를 생성하도록
+    //매개변수 autoGenerate을 true로 설정
+    @PrimaryKey(autoGenerate = true)
+    var nightId: Long = 0L,
+    //나머지 속성엔 @ColumnInfo 사용
+    @ColumnInfo(name = "start_time_milli")
+    val startTimeMilli: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "end_time_milli")
+    var endTimeMilli: Long = startTimeMilli,
+    @ColumnInfo(name = "quality_rating")
+    var sleepQuality: Int = -1
+)
