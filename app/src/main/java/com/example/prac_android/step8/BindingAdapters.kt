1f55af5488de58d3d99d187a37ce2bc627aa04a0
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-03-30 20:19:07 +0900

    [feat] error handling 추가

--- app/src/main/java/com/example/prac_android/step8/BindingAdapters.kt
+++ app/src/main/java/com/example/prac_android/step8/BindingAdapters.kt
@@ -1,5 +1,6 @@
 package com.example.prac_android.step8
 
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
@@ -8,6 +9,10 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.prac_android.R
 import com.example.prac_android.step8.network.MarsProperty
+import com.example.prac_android.step8.overview.MarsApiStatus
+import com.example.prac_android.step8.overview.MarsApiStatus.DONE
+import com.example.prac_android.step8.overview.MarsApiStatus.ERROR
+import com.example.prac_android.step8.overview.MarsApiStatus.LOADING
 import com.example.prac_android.step8.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")
@@ -28,4 +33,22 @@ fun bindImage(imgView: ImageView, imgUrl: String?) {
 fun bindRecyclerView(recyclerView: RecyclerView,data: List<MarsProperty>?) {
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView, status: MarsApiStatus?) {
+    when (status) {
+        LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        DONE -> {
+            statusImageView.visibility = View.GONE
+        }
+        else -> {}
+    }
 }
